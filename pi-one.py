032d982be15e65b07753a6ceac3c0163dc6111f4
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2022-08-11 15:02:07 +0000

    chosen

--- pi-one.py
+++ pi-one.py
@@ -3,30 +3,64 @@ import json
 from datetime import datetime
 import requests
 import time
+from multiprocessing import Process
+from legitindicators import super_smoother
 
 EXCHANGE_INFO = "https://api.binance.com/api/v3/exchangeInfo"
 KLINE_URL = "https://api.binance.com/api/v3/klines"
 
 KLINE_INTERVAL = "1d"
+KLINE_LIMIT = 30
+
 
 def main():
     exchange_info = get_exchange_info()
     pairs = get_pairs(exchange_info)
     busd_pairs = filter_busd_pairs(pairs)
+    for bp in busd_pairs:
+        p = Process(target=init_ops, args=(bp,))
+        p.start() 
+
+
+def init_ops(pair):
+    klines = get_kline(pair, KLINE_INTERVAL, KLINE_LIMIT)
+    open, high, _, close = get_ohlc(klines)
+    fall_short = 0
+    for i, _ in enumerate(klines):
+        if high[i] - open[i] < open[i] * 0.01:
+            fall_short = fall_short + 1
     
+    if fall_short == 0:
+        ss = super_smoother(close, 20)
+        if close[-1] > ss[-1]:
+            print(pair)
 
 
-def get_kline(kline_symbol, kline_interval):
+
+
+def get_ohlc(klines):
+    open = [float(o[1]) for o in klines]
+    high = [float(h[2]) for h in klines]
+    low = [float(l[3]) for l in klines]
+    close = [float(c[4]) for c in klines]
+    return open, high, low, close
+
+
+def get_high(klines):
+    high = [float(d[2]) for d in klines]
+    return high
+
+def get_kline(kline_symbol, kline_interval, kline_limit):
     try:
-        params = {"symbol": kline_symbol, "interval": kline_interval, "limit": 720}
+        params = {"symbol": kline_symbol, "interval": kline_interval, "limit": kline_limit}
         response = requests.get(
             url=f"{KLINE_URL}", params=params)
         response.raise_for_status()
         kline = response.json()
-        kline = kline[:-1]
+        # kline = kline[:-1]
         return kline
     except requests.exceptions.RequestException as err:
-        logging.error(err)
+        print(err)
         return None
 
 
