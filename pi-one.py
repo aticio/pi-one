2b5d03d9ae2fb83de8608777a03956668ea07449
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2022-08-22 15:09:18 +0000

    new approach

--- pi-one.py
+++ pi-one.py
@@ -9,6 +9,8 @@ import os
 import configparser
 import logging
 import logging.handlers
+import schedule
+import time
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 os.chdir(cwd)
@@ -20,85 +22,123 @@ cp.read(cwd + "/config.ini")
 
 BINANCE_URL = cp["context"]["BinanceUrl"]
 BINANCE_WEBSOCKET_ADDRESS = cp["context"]["BinanceWebSocketAddress"]
+KLINE_PATH = cp["context"]["KlinePath"]
 EXCHANGE_INFO = cp["context"]["ExchangeInfo"]
 SPOT_ORDER_PATH = cp["context"]["SpotOrderPath"]
 SPOT_ACCOUNT_PATH = cp["context"]["SpotAccountPath"]
 
-QUOTE = cp["data"]["Quote"]
+SYMBOL = ""
 BASE = ""
+QUOTE = ""
+STEP_SIZE = 0
 
 # Auth
 API_KEY = os.getenv("BINANCE_API_KEY")
 SECRET = os.getenv("BINANCE_API_SECRET")
 
-PRICE_DATA = {}
-STEP_SIZES = {}
-
-POS_PRICE = 0.0
 EXIT_PRICE = 0.0
-IN_POSITION = False
-SELECTED_PAIR = ""
+IN_STREAM = False
+
+W_S = None
 
 
 def main():
     configure_logs()
     logging.info("Happy trading.")
 
-    logging.info("Getting pairs")
-    exchange_info = get_exchange_info()
-    pairs = get_busd_pairs(exchange_info)
-    for pair in pairs:
-        PRICE_DATA[pair] = []
-        STEP_SIZES[pair] = 0
-
-    collect_step_sizes(exchange_info, pairs)
-
-    logging.info("Initiating websocket stream")
-    init_stream()
+    schedule.every(10).seconds.do(init_ops)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
-def get_busd_pairs(exchange_info):
-    pairs = []
-    for symbol in exchange_info["symbols"]:
-        if "BUSD" in symbol["symbol"] and "USDT" not in symbol["symbol"]:
-            pairs.append(symbol["symbol"])
-    return pairs
+def init_ops():
+    global cp
+    global EXIT_PRICE
+    global BINANCE_WEBSOCKET_ADDRESS
+    global IN_STREAM
+    global SYMBOL
+    global BASE
+    global QUOTE
+    global STEP_SIZE
+
+    cp.read(cwd + "/config.ini")
+
+    QUOTE = cp["data"]["Quote"]
+    SYMBOL = cp["data"]["Symbol"]
+    BASE = cp["data"]["Base"]
+    exit_ratio = float(cp["data"]["ExitRatio"])
+
+    BINANCE_WEBSOCKET_ADDRESS = BINANCE_WEBSOCKET_ADDRESS.replace("symbol", str.lower(SYMBOL))
+    print(BINANCE_WEBSOCKET_ADDRESS)
+
+    if SYMBOL != "":
+        # enter_long()
+        klines = get_klines(SYMBOL, "1d", 20)
+        _, _, _, close = get_ohlc(klines)
+        EXIT_PRICE = float(close[-1]) + (float(close[-1]) * exit_ratio)
+        exchange_info = get_exchange_info(SYMBOL)
+        STEP_SIZE = get_step_size(exchange_info)
+
+        if IN_STREAM is False:
+            IN_STREAM = True
+            init_stream()        
+    else:
+        logging.info("NO GO")
 
 
-def collect_step_sizes(exchange_info, pairs):
-    global STEP_SIZES
-
-    symbols = exchange_info["symbols"]
-    for symbol in symbols:
-        if "BUSD" in symbol["symbol"] and "USDT" not in symbol["symbol"]:
-            for filter in symbol["filters"]:
-                if filter["filterType"] == "LOT_SIZE":
-                    step = 0
-                    split_steps = filter["stepSize"].split(".")
-                    if split_steps[0] == "1":
-                        step = 0
-                    else:
-                        split_decimal = split_steps[1].find("1")
-                        step = split_decimal + 1
-                    STEP_SIZES[symbol["symbol"]] = step                
+def get_ohlc(klines):
+    open = [float(o[1]) for o in klines]
+    high = [float(h[2]) for h in klines]
+    low = [float(l[3]) for l in klines]
+    close = [float(c[4]) for c in klines]
+    return open, high, low, close
 
 
-def get_exchange_info():
-    response = requests.get(BINANCE_URL + EXCHANGE_INFO)
+def get_klines(kline_symbol, kline_interval, kline_limit):
+    try:
+        params = {"symbol": kline_symbol, "interval": kline_interval, "limit": kline_limit}
+        response = requests.get(
+            url=f"{BINANCE_URL}{KLINE_PATH}", params=params)
+        response.raise_for_status()
+        kline = response.json()
+        return kline
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return None
+
+
+def get_exchange_info(symbol):
+    response = requests.get(BINANCE_URL + EXCHANGE_INFO + f"?symbol={symbol}")
     exchange_info = response.json()
     return exchange_info
 
 
+def get_step_size(exchange_info):
+    step = 0
+    for filter in exchange_info["symbols"][0]["filters"]:
+        if filter["filterType"] == "LOT_SIZE":
+            split_steps = filter["stepSize"].split(".")
+            if split_steps[0] == "1":
+                step = 0
+            else:
+                split_decimal = split_steps[1].find("1")
+                step = split_decimal + 1
+            break         
+    return step
+
+
 # Websocket functions
 def init_stream():
-    w_s = websocket.WebSocketApp(
+    global W_S
+    W_S = websocket.WebSocketApp(
         BINANCE_WEBSOCKET_ADDRESS,
         on_message=on_message,
         on_error=on_error,
         on_close=on_close
         )
-    w_s.on_open = on_open
-    w_s.run_forever()
+    W_S.on_open = on_open
+    W_S.run_forever()
 
 
 def on_error(w_s, error):
@@ -106,8 +146,9 @@ def on_error(w_s, error):
 
 
 def on_close(w_s, close_status_code, close_msg):
-    print("closing websocket connection, initiating again...")
-    init_stream()
+    if IN_STREAM is True:
+        print("closing websocket connection, initiating again...")
+        init_stream()
 
 
 def on_open(w_s):
@@ -115,40 +156,16 @@ def on_open(w_s):
 
 
 def on_message(w_s, message):
-    global PRICE_DATA
-    global SELECTED_PAIR
-
-    ticker_data = json.loads(message)
-
-    for t in ticker_data:
-        if "BUSD" in t["s"] and "USDT" not in t["s"]:
-            PRICE_DATA[t["s"]].append(t["c"])
-            if len(PRICE_DATA[t["s"]]) == 600:
-                PRICE_DATA[t["s"]].pop(0)
-
-            anomaly = check_anomaly(PRICE_DATA[t["s"]])
-            if anomaly:
-                logging.info(f"Anomaly detected: {t['s']}: {t['c']}")
-                if IN_POSITION is False:
-                    SELECTED_PAIR = t["s"]
-                    enter_long(t["s"], t["c"])
-
-            if IN_POSITION is True:
-                if t["s"] == SELECTED_PAIR:
-                    if float(t["c"]) >= EXIT_PRICE:
-                        exit_long(t["s"], t["c"])
+    t = json.loads(message)
+    
+    if float(t["c"]) >= EXIT_PRICE:
+        exit_long()
                     
 
-def enter_long(symbol, price):
-    global POS_PRICE
-    global EXIT_PRICE
+def enter_long():
     global IN_POSITION
-    global BASE
 
-    BASE = symbol[:-4]
     IN_POSITION = True
-    POS_PRICE = price
-    EXIT_PRICE = price + (price * 0.01)
 
     logging.info("Opening long position.")
 
@@ -161,20 +178,25 @@ def enter_long(symbol, price):
     logging.info(f"Quote balance: {balance} {QUOTE}")
 
     order_response = spot_order_quote(
-        symbol,
+        SYMBOL,
         "BUY",
         "MARKET",
         algoutils.truncate_floor(balance, 6))
 
     if not order_response:
         return
+    
+    logging.info(json.dumps(order_response, sort_keys=True, indent=4))
 
 
-def exit_long(symbol, price):
-    global POS_PRICE
+def exit_long():
     global EXIT_PRICE
-    global IN_POSITION
     global BASE
+    global SYMBOL
+    global QUOTE
+    global IN_STREAM
+    global STEP_SIZE
+    global W_S
 
     logging.info("Closing long position")
 
@@ -184,33 +206,26 @@ def exit_long(symbol, price):
     if not balance:
         return
 
-    amount_to_sell = algoutils.truncate_floor(balance, STEP_SIZES[symbol])
-    if STEP_SIZES[symbol] == 0:
+    amount_to_sell = algoutils.truncate_floor(balance, STEP_SIZE)
+    if STEP_SIZE == 0:
         amount_to_sell = int(amount_to_sell)
     logging.info("Amount to sell %f %s", amount_to_sell, BASE)
 
-    spot_order_response = spot_order(symbol, "SELL", "MARKET", amount_to_sell)
+    spot_order_response = spot_order(SYMBOL, "SELL", "MARKET", amount_to_sell)
 
     if not spot_order_response:
         return
 
     logging.info("Sell order has been filled")
 
-    POS_PRICE = 0.0
-    EXIT_PRICE = 0.0
-    IN_POSITION = False
+    STEP_SIZE = 0
+    SYMBOL = ""
     BASE = ""
+    QUOTE = ""
+    EXIT_PRICE = 0.0
+    IN_STREAM = False
 
-
-def check_anomaly(prices):
-    anomaly = False
-    for p in prices[420:540]:
-        if float(prices[-1]) > (float(p) + (float(p) * 0.1)):
-            for p2 in prices[:540]:
-                if float(prices[-1]) < (float(p2) + (float(p2) * 0.2)):
-                    anomaly = True
-                    break
-    return anomaly
+    W_S.on_close()
 
 
 # Spot account trade functions
